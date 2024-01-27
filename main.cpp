8e03ae0ab1ed26629596e9a13ff126154797ba28
Saad-usm <77473248+Saad-usm@users.noreply.github.com>
2024-01-27 02:36:48 -0500

    test c++ code

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "functions.h"
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Sum: " << add(5, 3) << std::endl; // Example function call
+    return 0;
+}
